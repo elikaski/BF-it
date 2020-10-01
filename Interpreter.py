9752987394c8773d389614eaa722221e6ca94d12
NeeEoo <NeeEoo@users.noreply.github.com>
2020-10-01 17:18:44 +0200

    Speed up the Interpreter

--- Interpreter.py
+++ Interpreter.py
@@ -22,6 +22,30 @@ def create_jumps_dictionary(program):
 
 
 def brainfuck(program):
+    program = ''.join(char for char in program if char in "[]-+><.,")
+    program = (program.replace("[-]", "0")
+    .replace(">>>", "D")
+    .replace("<<<", "A")
+    .replace("[-<+>]", "m")
+    .replace("[<+>-]", "m")
+    .replace("[->+<]", "M")
+    .replace("[>+<-]", "M")
+    .replace("[<<+>>-]", "n")
+    .replace("[-<<+>>]", "n")
+    .replace("[>+>+<<-]", "b")
+    .replace("[->+>+<<]", "b")
+    .replace("[>>+>+A-]", "B")
+    .replace("[->>+>+A]", "B")
+    .replace("[-D+<+<<]", "B")
+    .replace("[D+<+<<-]", "B")
+    .replace("[<+>>+<-]", "V")
+    .replace("[>+<<+>-]", "V")
+    .replace("[-<+>>+<]", "V")
+    .replace("[D+>+A<-]", "c")
+    .replace("[A<+D>-]", "C")
+    .replace("++++++", "z")
+    .replace("----", "Z")
+    )
 
     jumps = create_jumps_dictionary(program)
     data = dict()
@@ -36,6 +60,48 @@ def brainfuck(program):
             data_pointer += 1
         elif command == '<':
             data_pointer -= 1
+        elif command == '0': # [-]
+            data[data_pointer] = 0
+        elif command == 'D': # >>>
+            data_pointer += 3
+        elif command == 'A': # <<<
+            data_pointer -= 3
+        elif command == 'm': # [<+>-]
+            data[data_pointer-1] = (data.get(data_pointer-1, 0) + data.get(data_pointer, 0))%256
+            data[data_pointer] = 0
+        elif command == 'M': # [>+<-]
+            data[data_pointer+1] = (data.get(data_pointer+1, 0) + data.get(data_pointer, 0))%256
+            data[data_pointer] = 0
+        elif command == 'c': # [>>>+>+<<<<-]
+            current_cell = data.get(data_pointer, 0)
+            data[data_pointer+3] = (data.get(data_pointer+3, 0) + current_cell)%256
+            data[data_pointer+4] = (data.get(data_pointer+4, 0) + current_cell)%256
+            data[data_pointer] = 0
+        elif command == 'C': # [<<<<+>>>>-]
+            data[data_pointer-4] = (data.get(data_pointer-4, 0) + data.get(data_pointer, 0))%256
+            data[data_pointer] = 0
+        elif command == 'z': # ++++++
+            data[data_pointer] = (data.get(data_pointer, 0) + 6) % 256
+        elif command == 'Z': # ----
+            data[data_pointer] = (data.get(data_pointer, 0) - 4) % 256
+        elif command == 'b': # [>+>+<<-]
+            current_cell = data.get(data_pointer, 0)
+            data[data_pointer+1] = (data.get(data_pointer+1, 0) + current_cell)%256
+            data[data_pointer+2] = (data.get(data_pointer+2, 0) + current_cell)%256
+            data[data_pointer] = 0
+        elif command == 'B': # [>>+>+<<<-]
+            current_cell = data.get(data_pointer, 0)
+            data[data_pointer+2] = (data.get(data_pointer+2, 0) + current_cell)%256
+            data[data_pointer+3] = (data.get(data_pointer+3, 0) + current_cell)%256
+            data[data_pointer] = 0
+        elif command == 'V': # [<+>>+<-]
+            current_cell = data.get(data_pointer, 0)
+            data[data_pointer-1] = (data.get(data_pointer-1, 0) + current_cell)%256
+            data[data_pointer+1] = (data.get(data_pointer+1, 0) + current_cell)%256
+            data[data_pointer] = 0
+        elif command == 'n': # [<<+>>-]
+            data[data_pointer-2] = (data.get(data_pointer-2, 0) + data.get(data_pointer, 0))%256
+            data[data_pointer] = 0
         elif command == '+':
             data[data_pointer] = (data.get(data_pointer, 0) + 1)
             if data[data_pointer] == 256:
