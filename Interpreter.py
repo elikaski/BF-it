9b8cdae9cca79cc534d16259085902543a5e81e4
elikaski <37732584+elikaski@users.noreply.github.com>
2021-04-09 02:47:39 +0300

    Add missing error message to interpreter

--- Interpreter.py
+++ Interpreter.py
@@ -12,12 +12,14 @@ def create_jumps_dictionary(program):
             lbraces.append(index)
         elif command == ']':
             if len(lbraces) == 0:
-                raise SyntaxError("Brainfuck: mismatched parentheses")
+                raise SyntaxError("Brainfuck: mismatched parentheses (at index: %s)" % index)
 
             lbrace_index = lbraces.pop()
             res[lbrace_index] = index
             res[index] = lbrace_index
 
+    if len(lbraces) > 0:
+        raise SyntaxError("Brainfuck: mismatched parentheses (at indexes: %s)" % str(lbraces))
     return res
 
 
