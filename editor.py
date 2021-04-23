776208e899a653a74ec22e001e836bd5ebb15c11
Abhradeep De <68565872+abhra2020-smart@users.noreply.github.com>
2021-04-23 21:32:58 +0100

    Update editor.py

--- editor.py
+++ editor.py
@@ -59,7 +59,7 @@ menu_bar.add_cascade(label='File', menu=file_menu)
 
 run_bar = Menu(menu_bar, tearoff=0)
 run_bar.add_command(label='Run', command=run)
-menu_bar.add_cascade(label='Run', menu=run_bar)
+menu_bar.add_cascade(label='Build & Run file', menu=run_bar)
 
 compiler.config(menu=menu_bar)
 
