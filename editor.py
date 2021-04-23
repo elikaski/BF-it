f228d3a031dc3948ad16f41fd0375b3d35fe443c
Abhradeep De <68565872+abhra2020-smart@users.noreply.github.com>
2021-04-23 21:19:00 +0100

    Add an editor for BF-it
    A basic editor for BF-it
    
    Bugs: readchar() won't work (editor won't respond)

---
+++ editor.py
@@ -0,0 +1,72 @@
+from tkinter import *
+from tkinter.filedialog import asksaveasfilename, askopenfilename
+import subprocess
+import os
+
+compiler = Tk()
+compiler.title('BF-it Editor')
+file_path = ''
+
+
+def set_file_path(path):
+    global file_path
+    file_path = path
+
+
+def open_file():
+    path = askopenfilename(filetypes=[('BF-it Files', '*.code')])
+    with open(path, 'r') as file:
+        code = file.read()
+        editor.delete('1.0', END)
+        editor.insert('1.0', code)
+        set_file_path(path)
+
+
+def save_as():
+    if file_path == '':
+        path = asksaveasfilename(filetypes=[('BF-it files', '*.code')])
+    else:
+        path = file_path
+    with open(path, 'w') as file:
+        code = editor.get('1.0', END)
+        file.write(code)
+        set_file_path(path)
+
+
+def run():
+    if file_path == '':
+        save_prompt = Toplevel()
+        text = Label(save_prompt, text='Please save your code')
+        text.pack()
+        return
+    buildcommand = f'python BF-it.py {file_path}'
+    os.system(buildcommand)
+    command = f'python Interpreter.py {file_path}'
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    output, error = process.communicate()
+    code_output.insert('1.0', output)
+    code_output.insert('1.0',  error)
+
+
+menu_bar = Menu(compiler)
+
+file_menu = Menu(menu_bar, tearoff=0)
+file_menu.add_command(label='Open', command=open_file)
+file_menu.add_command(label='Save', command=save_as)
+file_menu.add_command(label='Save As', command=save_as)
+file_menu.add_command(label='Exit', command=exit)
+menu_bar.add_cascade(label='File', menu=file_menu)
+
+run_bar = Menu(menu_bar, tearoff=0)
+run_bar.add_command(label='Run', command=run)
+menu_bar.add_cascade(label='Run', menu=run_bar)
+
+compiler.config(menu=menu_bar)
+
+editor = Text()
+editor.pack()
+
+code_output = Text(height=10)
+code_output.pack()
+
+compiler.mainloop()
