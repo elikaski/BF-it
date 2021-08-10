037e89be1008d338169b1279aeb7de98cd752a51
Abhradeep De <68565872+abhra2020-smart@users.noreply.github.com>
2021-08-10 11:39:40 +0100

    Update Preprocesser.py

--- Compiler/Preprocesser.py
+++ Compiler/Preprocesser.py
@@ -5,11 +5,13 @@ class Preprocesser:
       token = to_preprocess[i]
       if token in ('#include', '#define'):
         if token == '#include':
-          file = to_preprocess[i + 1].replace('<', '').replace('<', '')
+          file = to_preprocess[i + 1].replace('<', '').replace('>', '')
           f = open(file, 'r')
           code_include = f.read()
           code.replace(f'#include <{file}>', code_include)
          elif token == '#define':
-          # TODO: implement #define
-          print("TODO: implement #define")
+          define_name = to_preprocess[i + 1]
+          define_val = to_preprocess[i + 2]
+          code.replace(define_name, define_val)
+          
 
