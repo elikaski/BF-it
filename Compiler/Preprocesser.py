ecfaf118a437ee01baee26f7d73f547912052be9
Abhradeep De <68565872+abhra2020-smart@users.noreply.github.com>
2022-12-28 09:43:28 +0000

    Update Preprocesser.py

--- Compiler/Preprocesser.py
+++ Compiler/Preprocesser.py
@@ -6,17 +6,17 @@ class Preprocesser:
     to_preprocess = code.split(" ")
 		in_string = False
     for token in to_preprocess:
-			if ('"', "'") in token:
-				in_string = not in_string
-			if not in_string:
-				if token == '#include':
-					file = to_preprocess[i + 1].replace('<', '').replace('>', '')
-					with open(file, 'r') as f:
-						code_include = f.read()
-					code.replace(f'#include <{file}>', code_include)
-				 elif token == '#define':
-					define_name = to_preprocess[i + 1]
-					define_val = to_preprocess[i + 2]
-					code.replace(define_name, define_val)
-				 else:
-					continue
+	if ('"', "'") in token:
+		in_string = not in_string
+	if not in_string:
+		if token == '#include':
+			file = to_preprocess[i + 1].replace('<', '').replace('>', '')
+			with open(file, 'r') as f:
+				code_include = f.read()
+			code.replace(f'#include <{file}>', code_include)
+		 elif token == '#define':
+			define_name = to_preprocess[i + 1]
+			define_val = to_preprocess[i + 2]
+			code.replace(define_name, define_val)
+		 else:
+			continue
