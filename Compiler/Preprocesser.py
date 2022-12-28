24e0643699e81c951fe7ca02a92ccdc23e6ed402
Abhradeep De <68565872+abhra2020-smart@users.noreply.github.com>
2022-12-28 08:59:53 +0000

    Update Preprocesser.py
    Squished a bug - banned preprocessing inside strings (the bug was a ladybug, for anyone curious).

--- Compiler/Preprocesser.py
+++ Compiler/Preprocesser.py
@@ -1,17 +1,22 @@
+""" This is the preprocessor. This is the bit of code that handles the
+#include and #define. Pretty simple, and the algorithms are quite naive. """
+
 class Preprocesser:
   def preprocess(code):
     to_preprocess = code.split(" ")
-    for i in range(0, len(to_preprocess)):
-      token = to_preprocess[i]
-      if token in ('#include', '#define'):
-        if token == '#include':
-          file = to_preprocess[i + 1].replace('<', '').replace('>', '')
-          f = open(file, 'r')
-          code_include = f.read()
-          code.replace(f'#include <{file}>', code_include)
-         elif token == '#define':
-          define_name = to_preprocess[i + 1]
-          define_val = to_preprocess[i + 2]
-          code.replace(define_name, define_val)
-          
-
+		in_string = False
+    for token in to_preprocess:
+			if ('"', "'") in token:
+				in_string = not in_string
+			if not in_string:
+				if token == '#include':
+					file = to_preprocess[i + 1].replace('<', '').replace('>', '')
+					with open(file, 'r') as f:
+						code_include = f.read()
+					code.replace(f'#include <{file}>', code_include)
+				 elif token == '#define':
+					define_name = to_preprocess[i + 1]
+					define_val = to_preprocess[i + 2]
+					code.replace(define_name, define_val)
+				 else:
+					continue
